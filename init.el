24def7d0b6346d4a6b180683e63f4cfd86a9df92
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-02-07 13:47:14 +0900

    commentout skk

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 ;; M-x skk-tutorialでNo file found as 〜とエラーが出たときにskk-tut-fileを設定
 ;; make what-whereでSKK tutorialsで表示されるディレクトリ上のSKK.tutを指定
 ;;(setq skk-tut-file "/usr/share/skk/SKK.tut")
-::(require 'skk)
+;;::(require 'skk)
 
 ;;(global-set-key "\C-x\C-j" 'skk-mode)
 
