c176f6fa2621e07223bd6a85c7ebe74046e2edb8
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-09-17 16:51:13 +0900

    flymake -> flycheck

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(package-selected-packages
    (quote
-    (flycheck elpy smart-mode-line nyan-mode rainbow-delimiters))))
+    (elpygen smartrep helm-flycheck flycheck elpy smart-mode-line nyan-mode rainbow-delimiters))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -119,7 +119,13 @@
 (elpy-enable)
 (setq elpy-rpc-virtualenv-path 'current)
 
-;; use flycheck
+;;use flycheck
 (when (require 'flyckeck nil t)
   (remove-hook 'elpy-modules 'elpy-module-flymake)
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(define-key elpy-mode-map (kbd "C-c C-v") 'helm-flycheck)
+(require 'smartrep)   
+(smartrep-define-key elpy-mode-map "C-c"
+  '(("C-n" . flycheck-next-error)
+    ("C-p" . flycheck-previous-error)))
